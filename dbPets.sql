701552ee4d9949bc27041fae78f4fa7a9be0f208
jujumferreira <juliadasilvanferreira@gmail.com>
2024-08-02 20:17:49 -0300

    script banco 2

--- dbPets.sql
+++ dbPets.sql
@@ -6,23 +6,31 @@ use dbPets;
 -- visualizando banco de dados
 show databases;
 -- criando tabelas no banco de dados
+
+create table tbFuncionarios(
+codFun int not null auto_increment,
+nome varchar(100) not null,
+email varchar(100),
+telCel char(10),
+cpf char(14),
+primary key(codFun)
+);
+
 create table tbUsuarios(
 codUsu int not null auto_increment,
 nome varchar(50) not null,
 senha varchar(50)not null,
-primary key(codUsu)
+codFun int not null,
+primary key(codUsu),
+foreign key (codFun)references tbFuncionarios(codFun)
 );
 
-create table tbFuncionarios(
-codFun int not null auto_increment,
-nome varchar(100) not null
-email varchar(100),
-telCel char(10)
-);
+
 -- visualizando tabelas 
 show tables;
 -- visualizando a estrutura das tabelas
 desc tbUsuarios;
+desc tbFuncionarios;
 
 
 
