a4514a32c120450418c22289789995e2b1170c98
jujumferreira <juliadasilvanferreira@gmail.com>
2024-08-02 19:50:18 -0300

    script atualizado

--- dbPets.sql
+++ dbPets.sql
@@ -5,5 +5,24 @@ create database dbPets;
 use dbPets;
 -- visualizando banco de dados
 show databases;
+-- criando tabelas no banco de dados
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nome varchar(50) not null,
+senha varchar(50)not null,
+primary key(codUsu)
+);
+
+create table tbFuncionarios(
+codFun int not null auto_increment,
+nome varchar(100) not null
+email varchar(100),
+telCel char(10)
+);
+-- visualizando tabelas 
+show tables;
+-- visualizando a estrutura das tabelas
+desc tbUsuarios;
+
 
 
